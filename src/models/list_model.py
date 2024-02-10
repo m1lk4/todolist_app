60caa5f0f92a0813b3c5bdcd9f8a71a3c5847273
Milka Sofia Ivanoff <ivanoffmilka@gmail.com>
2024-02-10 23:36:37 +0100

    Agrego clase para las Listas

---
+++ src/models/list_model.py
@@ -0,0 +1,7 @@
+from src.database.todolist_db import db
+
+
+class List(db.Model):
+    __tablename__ = "lists"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
