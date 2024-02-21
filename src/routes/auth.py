f7d8edce8890bf23ab4036d6a757b7ec73b44c56
Milka Sofia Ivanoff <ivanoffmilka@gmail.com>
2024-02-21 23:05:56 +0100

    Vista del login y signup

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,3 +1,16 @@
 from flask import Blueprint
+from flask import render_template
 
-auth_blueprint = Blueprint("auth", __name__, url_prefix="/auth")
+auth_blueprint = Blueprint("auth", __name__)
+
+
+@auth_blueprint.get("/signup")
+def signup():
+
+    return render_template("auth/signup.html")
+
+
+@auth_blueprint.get("/login")
+def login():
+
+    return render_template("auth/login.html")
