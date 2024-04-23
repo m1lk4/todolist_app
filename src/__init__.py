1863f9c0cdf1be66e1f7ed61f5bb9d84becf23f5
Milka Sofia Ivanoff <ivanoffmilka@gmail.com>
2024-04-23 16:43:42 +0200

    Modifico configuraciones de entorno

--- src/__init__.py
+++ src/__init__.py
@@ -5,18 +5,20 @@ from src.routes.task import task_blueprint
 from src.routes.auth import auth_blueprint
 from src.routes.tag import tag_blueprint
 from src.routes import home_blueprint
-from src.config import ConfigDev
+from src.config import config
 from src.database.todolist import db
 
 
-def create_app(static_folder="static"):
+def create_app(env="development", static_folder="static"):
     app = Flask(__name__, static_folder=static_folder)
-    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/1'
 
-    app.config.from_object(ConfigDev)
+    # Loading config
+    app.config.from_object(config[env])
 
+    # Init database
     db.init_app(app)
 
+    # Controllers
     app.register_blueprint(list_blueprint)
     app.register_blueprint(task_blueprint)
     app.register_blueprint(auth_blueprint)
--- src/config.py
+++ src/config.py
@@ -1,4 +1,41 @@
-class ConfigDev:
+from os import environ
+
+
+# Base configuration
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/1'
+    SESSION_COOKIE_SECURE = True
+
+
+# Production configuration
+class ProductionConfig(Config):
+    DB_USER = environ.get("DB_USER")
+    DB_PASS = environ.get("DB_PASS")
+    DB_HOST = environ.get("DB_HOST")
+    DB_NAME = environ.get("DB_NAME")
+    SESSION_TYPE = "filesystem"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost:15432/todolist"
+
+
+# Development configuration
+class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost:15432/todolist"
+    SESSION_TYPE = "filesystem"
+    SESSION_COOKIE_SECURE = False
+
+
+# Testing configuration
+class TestingConfig(Config):
+    TESTING = True
+
+
+config = {
+    "development": DevelopmentConfig,
+    "test": TestingConfig,
+    "production": ProductionConfig,
+}
